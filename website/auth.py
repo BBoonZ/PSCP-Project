82ed3dd7f04f07f3feddb07beec0664f517cd496
BBoonZ <bosszaza0380@gmail.com>
2023-09-25 17:19:30 +0700

    web 0.1

---
+++ website/auth.py
@@ -0,0 +1,19 @@
+from flask import Blueprint ,render_template, request
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    print(data)
+    return render_template('login.html')
+
+@auth.route('/logout')
+def logout():
+    return '<p>logout</p>'
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        print('true')
+    return render_template('signup.html')
