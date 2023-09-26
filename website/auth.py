d14ec1ca2d0c0b0e3e6e8500f9a008d4b6263135
realnena <nong.nena03@gmail.com>
2023-09-26 15:18:57 +0700

    login_success

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,35 @@
-from flask import Blueprint ,render_template, request, flash
+from flask import Blueprint ,render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from . import db
+
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.test'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template('login2.html')
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return '<p>logout</p>'
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -27,5 +46,10 @@ def sign_up():
             flash('Password must be at least than 7 characters', category='error')
         else:
             # add user to database
+            new_user = User(email=email, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
             flash('Account have been create', category='success')
+            return redirect(url_for('views.home'))
     return render_template('sign-up.html')
--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import logout_user, current_user
+
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template('base.html')
+
+@views.route('/test')
+def test():
+    return render_template('test.html')
