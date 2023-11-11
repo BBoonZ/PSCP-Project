33094fbfbab2b6fdb437851b0c7598ee91470e7a
BBoonZ <bosszaza0380@gmail.com>
2023-11-11 17:13:43 +0700

    version 1.0

---
+++ website/auth.py
@@ -0,0 +1,197 @@
+from flask import Blueprint ,render_template, request, flash, redirect, url_for,current_app, Flask
+from .models import User, Note, Pic
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from . import db, mail
+from flask_mail import Mail,Message
+from itsdangerous import URLSafeTimedSerializer
+import itsdangerous
+import datetime
+
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user and user.is_verified:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('auth.login'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        elif user and not user.is_verified:
+            flash('Please verify your email before logging in.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template('login2.html', user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be grater than 3 characters', category='error')
+        elif password1 != password2:
+            flash('Password don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least than 7 characters', category='error')
+        else:
+            # add user to database
+            new_user = User(email=email, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
+            serializer = current_app.serializer
+            token = serializer.dumps(email, salt='email-confirm')
+
+            new_user.verification_token = token
+            db.session.commit()
+            # send verification email
+            verification_url = url_for('auth.verify_email', token=token, _external=True)
+            message_body = f'Please click the following link to verify your email: {verification_url}'
+            send_verifly(email, message_body)
+
+            flash('Account have been create You have to verify at your email', category='success')
+            return redirect(url_for('auth.sign_up'))
+    return render_template('sign-up.html', user=current_user)
+
+def send_verifly(email, message):
+    msg = Message('Verify your email', sender='your_email@example.com', recipients=[email])
+    msg.body = message
+    mail.send(msg)
+
+@auth.route('/verify-email/<token>', methods=['GET'])
+def verify_email(token):
+    try:
+        serializer = current_app.serializer
+        email = serializer.loads(token, salt='email-confirm', max_age=3600)  # max_age in seconds
+        user = User.query.filter_by(email=email).first()
+        if user:
+            user.is_verified = True
+            db.session.commit()
+            flash('Email verified successfully.', category='success')
+        else:
+            flash('Invalid token or user not found.', category='error')
+
+    except Exception as e:
+        flash('Invalid or expired token.', category='error')
+
+    return redirect(url_for('auth.login'))
+
+def send_mail(email, reset_url):
+    msg = Message('Reset Password', sender='your_email@example.com', recipients=[email])
+    msg.body = f'To reset your password, please follow the link: {reset_url}'
+    mail.send(msg)
+
+@auth.route('/forgot', methods=['GET', 'POST'])
+def forgot():
+        if request.method == 'POST':
+            email = request.form.get('email')
+            user = User.query.filter_by(email=email).first()
+            if user:
+                serializer = current_app.serializer
+                token = serializer.dumps(email, salt='create-new-password')
+                # สร้าง URL สำหรับรีเซ็ตรหัสผ่าน
+                reset_url = url_for('auth.newpass', token=token, _external=True)
+                send_mail(email, reset_url)
+                flash('An email with instructions to reset your password has been sent to your email address.')
+                return redirect(url_for('auth.login'))
+            else:
+                "don't have your mail"
+
+    
+        return render_template('forgot.html', user=current_user)
+
+@auth.route('/create-new-password/<token>', methods=['GET', 'POST'])
+def newpass(token):
+    serializer = current_app.serializer
+    try:
+        email = serializer.loads(token, salt='create-new-password', max_age=3600)
+    except itsdangerous.BadSignature:
+        flash('The reset password link is invalid.')
+        return redirect(url_for('auth.login'))
+    except itsdangerous.SignatureExpired:
+        flash('The reset password link has expired.')
+        return redirect(url_for('auth.login'))
+    
+    if request.method == 'POST':
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if email:
+            user = User.query.filter_by(email=email).first()
+            if user:
+                if password1 is None or password2 is None:
+                    flash('Please enter a password.', category='error')
+                elif password1 != password2:
+                    flash('Passwords don\'t match.', category='error')
+                elif len(password1) < 7:
+                    flash('Password must be at least 7 characters', category='error')
+                else:
+                    # รับรหัสผ่านใหม่จากฟอร์ม
+                    # อัปเดตรหัสผ่านใหม่ในฐานข้อมูล
+                    user.password = generate_password_hash(password1, method='sha256')
+                    db.session.commit()
+                    flash('Your password has been reset successfully. You can now log in with your new password.')
+                    return redirect(url_for('auth.login'))
+        
+    return render_template('newpass.html', user=current_user)
+
+@auth.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    if request.method == 'POST':
+        note = request.form.get('text')
+        print(note)
+
+        file = request.files['file']
+        print(file)
+        if not file:
+            print('maimee')
+            pic = Pic.query.filter_by(id=1).all()[0]
+            upload = Pic(filename=pic.filename, data=pic.data, user_id=current_user.id)
+            file.filename = pic.filename
+        else:
+            upload = Pic(filename=file.filename, data=file.read(), user_id=current_user.id)
+        db.session.add(upload)
+        db.session.commit()
+
+        current_datetime = datetime.datetime.today()
+        month_name = current_datetime.strftime('%B')
+        day_name = current_datetime.strftime('%A')
+        _txt = Note(data=note, day=day_name, month=month_name, love='True', filename=file.filename, user_id=current_user.id)
+        db.session.add(_txt)
+        db.session.commit()
+        flash('Your Diary has been Create!', category='success')
+    return render_template('edit.html', user=current_user)
+
+'''@auth.route('/test2', methods=['GET', 'POST'])
+@login_required
+def test2():
+    if request.method == 'POST':
+        file = request.files['file']
+        print(file)
+
+        upload = Pic(filename=file.filename, data=file.read())
+        db.session.add(upload)
+        db.session.commit()
+
+    return render_template('test2.html', user=current_user)'''
---
+++ website/views.py
@@ -0,0 +1,89 @@
+from flask import Blueprint, render_template, send_file
+from io import BytesIO
+import base64
+
+from flask_login import login_required, current_user
+from .models import Note, Pic
+from . import db
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template('base.html', user=current_user)
+
+@views.route('/home')
+def home2():
+    return render_template('navbar.html', user=current_user)
+
+@views.route('/history/<month>')
+@login_required
+def history(month):
+    if month == 'none':
+        val = Note.query.filter_by(user_id=current_user.id).all()
+    else:
+        val = Note.query.filter_by(user_id=current_user.id, month=month).all()
+    val = val[::-1]
+    val_str = [str(i) for i in val]
+    val = zip(val_str, val)
+    print(val)
+    return render_template('history.html', user=current_user, note=val, val=len(val_str))
+
+@views.route('/history/diary/<note_id>')
+@login_required
+def history_2(note_id):
+    note = Note.query.filter_by(user_id=current_user.id).all()
+    note_str = [str(i) for i in note]
+    note_date = [str(i.date).split()[0].split('-')[2] for i in note]
+    for i in note:
+        i = str(i.date).split()[0].split('-')[2]
+        print(i)
+    return render_template(
+        'history_2.html',
+        user=current_user,
+        note_id=note_id, 
+        note=note,
+        note_len=len(note_str),
+        note_str=note_str,
+        note_date=note_date
+    )
+
+@views.route('/history/diary/')
+@login_required
+def history_error():
+    return render_template('history.html', user=current_user)
+
+@views.route('/diary')
+@login_required
+def diary():
+    val = Note.query.filter_by(user_id=current_user.id).all()
+    if val:
+        val = val[::-1][0]
+        val_date = str(val.date).split()[0].split('-')[2]
+        val_month = val.month
+        val_file = val.filename
+        return render_template('diary.html', user=current_user, note=val, val_month=val_month, val_date=val_date, val_file=val_file)
+    else:
+        val_file = 'none'
+        return render_template('diary.html', user=current_user, val_file=val_file)
+    return render_template('diary.html', user=current_user)
+
+@views.route('/test2')
+@login_required
+def test2():
+    return render_template('test.html', user=current_user)
+
+@views.route('/test/<filename>')
+@login_required
+def test(filename):
+    if filename == 'none':
+        pic = Pic.query.filter_by(id=4).all()[0]
+        return send_file(BytesIO(pic.data), download_name=pic.filename, as_attachment=True)
+    pic = Pic.query.filter_by(filename=filename).all()[0]
+    print(pic)
+    return send_file(BytesIO(pic.data), download_name=pic.filename, as_attachment=True)
+
+@views.route('add-db')
+def add_db():
+
+    return render_template('test3.html', user=current_user)
